ad258596c976ef0ed88031f2226bc8a39a5e6831
Ayomide Francis - Akinlolu <fayomide324@gmail.com>
2022-06-11 12:16:26 -0700

    Updated the base.html

--- base/urls.py
+++ base/urls.py
@@ -31,3 +31,7 @@ urlpatterns = [
 
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+admin.site.site_header = "Optimum Admin"
+admin.site.site_title = "Optimum News Admin Portal"
+admin.site.index_title = "Welcome to Optimum Admin Website"
