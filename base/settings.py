9ec081f13daafc1695a141e81e4146998dc95dea
Ayomide Francis - Akinlolu <fayomide324@gmail.com>
2022-06-10 22:19:24 -0700

    Adding heroku deploying files

--- base/settings.py
+++ base/settings.py
@@ -47,12 +47,15 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    # White noise
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'base.urls'
